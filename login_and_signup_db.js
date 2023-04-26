7e904a4dac993b41e8b5cf9940c5d07c1f134877
BolaGhaly <bolaghaly63@gmail.com>
2023-04-26 16:34:19 -0400

    Update signup endpoint

--- login_and_signup_db.js
+++ login_and_signup_db.js
@@ -1,58 +1,54 @@
-//connect database
 require("dotenv").config();
 const { emailValidation, passwordValidation } = require("./validation");
 const bcrypt = require("bcryptjs");
-const Pool = require("pg").Pool;
+const { createClient } = require("@supabase/supabase-js");
 
-const pool = new Pool({
-  user: `${process.env.DB_USER}`,
-  password: `${process.env.DB_PASSWORD}`,
-  host: `${process.env.DB_HOST}`,
-  port: process.env.DB_PORT,
-  database: `${process.env.DB_DATABASE}`,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
-});
+const url = process.env.SUPA_BASE_URL;
+const key = process.env.SUPA_BASE_KEY;
+const supabase = createClient(url, key);
 
 const createUser = async (request, response) => {
-  try {
-    const { username, email, password, currency } = request.body;
-    let errors = {};
+  const { username, email, password, accountBalance } = request.body;
+  let errors = {};
 
-    if (!emailValidation(email)) {
-      errors.email = "Email is not valid";
-    }
-    if (!passwordValidation(password)) {
-      errors.password = "Password is not valid";
-    }
-
-    const isEmailInUse = await pool.query(
-      "SELECT * FROM users WHERE email = $1",
-      [email]
-    );
-
-    if (isEmailInUse.rows.length > 0) {
-      errors.email = "Email is already in use";
-    }
-
-    if (Object.keys(errors).length > 0) {
-      return response.status(400).json(errors);
-    }
-
-    const salt = await bcrypt.genSalt(10); //salt encryption
-    const hashedPassword = await bcrypt.hash(password, salt); //hash encryption
-
-    const newUser = await pool.query(
-      "INSERT INTO users (username, email, password, currency) VALUES ($1, $2, $3, $4) RETURNING *",
-      [username, email, hashedPassword, currency]
-    ); //what this does: hey so you got the username, email, and password, i want you to
-    // run a query on the database so that it inserts this information using the sql in the quotes
-    response.json({ success: true, data: newUser.rows[0] });
-  } catch (error) {
-    response.status(500).json({ error: error.message });
+  if (!emailValidation(email)) {
+    errors.email = "Email is not valid";
   }
+  if (!passwordValidation(password)) {
+    errors.password = "Password is not valid";
+  }
+
+  const isUsernameInUse = await supabase
+    .from("users")
+    .select()
+    .eq("username", username);
+  if (isUsernameInUse.data.length > 0) {
+    errors.username = "Username is already in use";
+  }
+
+  const isEmailInUse = await supabase.from("users").select().eq("email", email);
+  if (isEmailInUse.data.length > 0) {
+    errors.email = "Email is already in use";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return response.status(400).json(errors);
+  }
+
+  const salt = await bcrypt.genSalt(10); //salt encryption
+  const hashedPassword = await bcrypt.hash(password, salt); //hash encryption
+
+  const { data, error } = await supabase
+    .from("users")
+    .insert({ username, email, password: hashedPassword, accountBalance })
+    .select();
+
+  if (error) {
+    alert(error.message);
+    return; // abort
+  }
+
+  response.json(data);
 };
 
 const login = async (request, response) => {
